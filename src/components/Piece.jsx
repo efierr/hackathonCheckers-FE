f70d6df8699388f867834d724b2553e0a97f80c1
Darrion117 <dcrowder@pursuit.org>
2024-12-10 18:03:04 -0500

    css and react. piece changes

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import red from "../assets/red.png"
+import yellow from "../assets/yellow.png"
 
 const Piece = ({ color, isKing }) => {
   return (
-    <div className={`piece ${color} ${isKing ? "king" : ""}`}>
-      {isKing && <span className="crown">♔</span>}
+    <div class= "piece">
+    {color === "red" ? <img src={red} alt="red" /> : <img src={yellow} alt="yellow" />}
     </div>
+    // <div className={`piece ${color} ${isKing ? "king" : ""}`}>
+    //   {isKing && <span className="crown">♔</span>}
+    // </div>
   );
 };
 
